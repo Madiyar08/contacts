1f795fdaed4aee7525e8fc728a7e6aae44e07008
Madiyar08 <abdusamatovmadiar@gmail.com>
2024-12-25 11:05:57 +0500

    upd ed

--- src/App.js
+++ src/App.js
@@ -3,10 +3,7 @@ import Main from "./Main";
 import Login from "./components/Login";
 import CreateMarket from "./pages/CreateMarket";
 import LoginForm from "./pages/LoginForm";
-<<<<<<< HEAD
-=======
 import Promos from "./pages/Promos";
->>>>>>> c1072c2dce332dbc8b4eb1165fa950ff5e6d144a
 import Request from "./components/Request";
 
 function App() {
@@ -17,12 +14,8 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/createMarket" element={<CreateMarket />} />
         <Route path="/loginform" element={<LoginForm />} />
-<<<<<<< HEAD
         <Route path="/requestpro" element={<Request />} />
-=======
         <Route path="/promos" element={<Promos />} />
-        <Route path="/requestpro" element={<Request />} />ё
->>>>>>> c1072c2dce332dbc8b4eb1165fa950ff5e6d144a
       </Routes>
     </BrowserRouter>
   );
